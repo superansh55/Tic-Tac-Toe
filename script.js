4fd18d10dbf5c75548281a81c9218e8bd16f42b2
superansh55 <superansh55@gmail.com>
2025-09-25 22:52:53 +0530

    added full winnner check logic

--- script.js
+++ script.js
@@ -11,21 +11,17 @@ const ansh = createPlayer("ansh", "x");
 
 
 const gameBoard =(function () {
- let board=   [
-  [0, 0, 0],
-  [0, 0, 0],
-  [0, 0, 0],
-];
+ let board=   [0,0,0,0,0,0,0,0,0];
  const getBoard= ()=> board;
-  const pushSymbol = (symbol,index1, index2) => {
-    if (board[index1][index2] != 0) {
+  const pushSymbol = (symbol,index) => {
+    if (board[index] != 0) {
       return;
     } else {
-      board[index1][index2] = symbol;
+      board[index] = symbol;
       if (check() !== undefined) {
-        console.log(check());
+           check();
       }
-      console.log(board);
+    
     }
   };
   return{pushSymbol,getBoard};
@@ -33,45 +29,49 @@ const gameBoard =(function () {
 
 function check() {
     const checkBoard= gameBoard.getBoard();
-    const loopBoard=()=>{
-       let countX1=0;
-       let countO1=0;
-       let countX2=0;
-        let countO2=0;
-       let  countX3=0;
-       let countO3=0;
-        function checkStraight(index,number1,number2){
-               checkBoard[index].forEach(item=>{
-        
-            if(item==="x"){
-                number1++;
-            }else{
-                number2++;
+    const checkWin1= (startIndex,EndIndex)=>{
+        countX=0;
+        countO=0;
+        for(let i = startIndex; i<EndIndex;i++){
+            if(checkBoard[i]==="x"){
+                countX++;
+            }else if(checkBoard[i]==="o"){
+                countO++;
             }
-        
-    })
-}
-        
-        checkStraight(0,countX1,countO1);
-        checkStraight(1,countX2,countO2);
-        checkStraight(2,countX3,countO3);
- 
-    if(countX1===3 || countX2===3 || countX3===3 ){
-        return "x";
-    }else if(countO1===3 || countO2===3 || countO3===3){
-        return "o";
+        }
+        if(countX===3){
+            console.log("p1 win");
+        }else if(countO===3){
+            console.log("p2 win");
+        }else{
+            return;
+        }
     }
-}
-  if (loopBoard()==="x") {
-    return "winner player one";
-  } else if ( loopBoard()==="o") {
-    return "winner player two";
-  }
+    const checkWin2=(index1,index2,index3)=>{
+        if(checkBoard[index1]==="x" && checkBoard[index2]==="x" && checkBoard[index3]==="x" ){
+             console.log("p1 win");
+        } else  if(checkBoard[index1]==="o" && checkBoard[index2]==="o" && checkBoard[index3]==="o" ){
+             console.log("p2 win");
+        } else{
+            return;
+        }
+    }
+
+    checkWin1(0,3);
+    checkWin1(3,6);
+    checkWin1(6,9);
+    checkWin2(0,3,6);
+    checkWin2(1,4,7);
+    checkWin2(2,5,8);
+    checkWin2(0,4,8);
+    checkWin2(2,4,6);
 }
 
-gameBoard.pushSymbol(ansh.playerSymbol,1,0);
-gameBoard.pushSymbol(ansh.playerSymbol,1,1);
-gameBoard.pushSymbol(ansh.playerSymbol,1,2);
+gameBoard.pushSymbol(ansh.playerSymbol,0);
+gameBoard.pushSymbol(ansh.playerSymbol,3);
+gameBoard.pushSymbol(ansh.playerSymbol,6);
+
+
 
 
 
