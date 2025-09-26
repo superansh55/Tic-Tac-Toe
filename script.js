c2eb40c8d2aeae80e8aebb5bb5463ec294f01fc9
superansh55 <superansh55@gmail.com>
2025-09-26 17:47:00 +0530

    added functionality to display scores along with winner best out of 3 rounds, fixed various game logic bugs

--- script.js
+++ script.js
@@ -14,22 +14,30 @@ const gameBoard =(function () {
  const getBoard= ()=> board;
   const pushSymbol = (symbol,index) => {
     if (board[index] != 0) {
-      return;
+      return "error";
     } else {
       board[index] = symbol;
-      if (check() !== undefined) {
-           check();
-      }
+   
     
     }
   };
-  return{pushSymbol,getBoard};
+
+  const clearBoard=()=>{
+
+    for(let i=0; i<board.length;i++){
+      board[i]=0;
+    }
+    
+  }
+  return{pushSymbol,getBoard,clearBoard};
 })();
 
-function check() {
-    const checkBoard= gameBoard.getBoard();
-    let p1Score=0;
+const checkWinner=(function (){
+   let p1Score=0;
     let p2Score=0;
+  function check(){
+    const checkBoard= gameBoard.getBoard();
+   
     const checkWin1= (startIndex,EndIndex)=>{
        let countX=0;
        let countO=0;
@@ -68,48 +76,102 @@ let winner=
   checkWin2(2, 4, 6);
  const isBoardFull = checkBoard.every(item => item !== 0);
   if(winner==="x"){
-    console.log("p1 wins");
-    p1Score++;
+     p1Score++;
+    return "p1";
+  
+   
   }else if(winner==="o"){
-    console.log("p2 wins");
     p2Score++;
+    return "p2";
+    
   }else if(isBoardFull){
-    console.log("tie");
+    return "tie";
 
+  }else if(winner==="error"){
+    return "error";
   }
+
+
   if(p1Score===3){
-    console.log("P1 is the winner");
+    return "P1 win";
   }else if(p2Score===3){
-    console.log("P2 is the winner");
+     return "P2 win";
   }
+
+
+ 
 }
+  const getP1Score=()=>p1Score;
+  const getP2Score=()=>p2Score;
+ return{check,getP1Score,getP2Score};
+})();
 
 
 const gameDisplay=(function() {
     const displayBoard=gameBoard.getBoard();
+    
     const gameScreen= document.querySelector(".gameScreen");
     const p1Name= document.querySelector("#p1Name");
     const p2Name= document.querySelector("#p2Name");
     const p1DisplayName=document.querySelector("#p1DisplayName");
       const p2DisplayName=document.querySelector("#p2DisplayName");
+    const winner=document.querySelector("#winner");
+  
     let pSymbol="x";
+ 
+    const p1Score=document.querySelector("#p1Score");
+    const p2Score=document.querySelector("#p2Score");
+ 
     const changeDisplay=()=>{
     for(let i=0; i<9 ;i++){
         const newDiv=document.createElement("div");
         newDiv.id=i;
         newDiv.classList.add("pattern_block");
+         newDiv.classList.add("numberDiv");
         gameScreen.appendChild(newDiv);
+          const clearDisplay=()=>{
+    gameBoard.clearBoard();
+   
+      const selectDiv= document.querySelectorAll(".numberDiv");
+      selectDiv.forEach(item=>item.innerText="");
+    
+   }
 
         newDiv.addEventListener("click",(event)=>{
+            const chkWnr=checkWinner.check();
+               const p1Scr=checkWinner.getP1Score();
+    const p2Scr=checkWinner.getP2Score();
           const divId =Number(event.target.id);
-          gameBoard.pushSymbol(pSymbol,divId);
+         const pushMark= gameBoard.pushSymbol(pSymbol,divId);
            newDiv.innerText=displayBoard[i];
+          if(pushMark!="error"){
            if(pSymbol==="x"){
             pSymbol="o";
            }else{
             pSymbol="x";
            }
+          }
+          
           console.log(displayBoard);
+          if(chkWnr==="p1"){
+         
+          p1Score.innerText=p1Scr;
+          clearDisplay();
+        
+        } else if(chkWnr==="p2"){
+       
+          p2Score.innerText=p2Scr;
+          clearDisplay();
+        } else if(chkWnr==="tie"){
+          winner.innerText="Its a Tie";
+          clearDisplay();
+        } else if(chkWnr==="P1 win"){
+           winner.innerText=`${p1DisplayName.textContent} Wins`;
+           clearDisplay();
+        }else if(chkWnr==="P2 win"){
+           winner.innerText=`${p2DisplayName.textContent} Wins`;
+           clearDisplay();
+        }
         })
     }
   }
@@ -128,6 +190,9 @@ const gameDisplay=(function() {
         createAndUpdatePlayer(p2Name,p2DisplayName);
     }
   })
+
+ 
+
 
 })();
 
